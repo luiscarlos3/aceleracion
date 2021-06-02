64ac77f985c8e3726007b1f0ec296b6b9ed4176a
luiscarlos27 <lszondas@gmail.com>
2021-06-01 22:27:28 -0500

    Initial commit

---
+++ aceleracion.py
@@ -0,0 +1,5 @@
+VA = float(input("Ingrese la variacion de la velociad : "))
+AT = float(input("Ingrese tiempo de variacion : "))
+VF = float(input("ingrese la velocidad final : "))
+var = (VA - AT)/ VF
+print("la acelaracion inicial es de : " , var)
